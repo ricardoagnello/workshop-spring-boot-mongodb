90c13a116d2856a9a03559798043b70d3159c3e5
Ricardo <ricardo.agnello@outlook.com>
2020-05-02 20:26:43 -0300

    Atualização de usuário com PUT

--- src/main/java/com/neutronstar/workshopmongo/resources/UserResource.java
+++ src/main/java/com/neutronstar/workshopmongo/resources/UserResource.java
@@ -52,4 +52,14 @@ public class UserResource {
 	
 	
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void>insert(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		}
+	
+
 }
