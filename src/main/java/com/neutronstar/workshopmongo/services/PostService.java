4063224718d45b00a7c99d6d058068e9c8e71071
Ricardo <ricardo.agnello@outlook.com>
2020-05-03 13:20:25 -0300

    Consulta simples com @Query

--- src/main/java/com/neutronstar/workshopmongo/services/PostService.java
+++ src/main/java/com/neutronstar/workshopmongo/services/PostService.java
@@ -12,19 +12,17 @@ import com.neutronstar.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
-	
+
 	@Autowired
 	private PostRepository repo;
-	
+
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
-	
-	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
 	}
-	
-	
+
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
 
 }
